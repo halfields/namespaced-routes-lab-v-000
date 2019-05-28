544925a811b3621fd445c18f5f28c1a47d9c15b2
halfields <hfields697@aol.com>
2019-05-28 15:36:42 -0500

    done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,19 +1,30 @@
 class ArtistsController < ApplicationController
+  before_action  :set_artist, only: [:show, :edit, :update, :destroy]
+
+
   def index
-    @artists = Artist.all
+    @preferences = Preference.first
+    if @preferences && @preferences.artist_sort_order
+      @artists = Artist.order(namae: @preferences.artist_sort_order)
+    else
+      @artists = Artist.all 
+    end
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def new
-    @artist = Artist.new
+    @preferences = Preference.first
+    if @preferences && !@preferences.allow_create_artists
+      redirect_to artists_path
+    else
+      @artist = Artist.new
+    end
   end
 
   def create
     @artist = Artist.new(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
@@ -22,14 +33,10 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
-
     @artist.update(artist_params)
-
     if @artist.save
       redirect_to @artist
     else
@@ -38,7 +45,6 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
@@ -49,4 +55,10 @@ class ArtistsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name)
   end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+
 end
